bf8639ac995c3c6f6a6b25be16546211141ea4a9
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-05-24 19:41:22 -0700

    Removing enabled/disabled button

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,58 +1,33 @@
 // Wrap the entire script inside an immediately-invoked function expression (IIFE) to avoid polluting the global scope
 (() => {
+  // Initial state
+  chrome.storage.local.set({ isSkipperEnabled: true });
+
   // Add an event listener for when a tab is updated
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Check if the update is complete and the tab URL includes 'https://www.youtube.com/'
-    if (
-      changeInfo.status === 'complete' &&
-      tab.url &&
-      tab.url.includes('https://www.youtube.com/')
-    ) {
-      // Fetch the 'enabled' value from storage
-      chrome.storage.sync.get('enabled', function(data) {
-        const enabled = data.enabled;
-        // Only execute the script if the 'enabled' value is true
-        if (enabled) {
-          // Execute a script in the updated tab
-          chrome.scripting.executeScript(
-            {
-              target: { tabId },
-              func: function () {
-                if (!window.myExtensionScriptInjected) {
-                  // Set a flag to indicate that the script has been injected
-                  window.myExtensionScriptInjected = true;
-
-                  // Define a function to click an element
-                  function clickElement() {
-                    // Find the 'Skip' button element
-                    var element = document.querySelector(
-                      '.ytp-ad-skip-button.ytp-button'
-                    );
-                    // Check if the element exists and is visible
-                    if (element && element.offsetParent !== null) {
-                      // Click the element
-                      element.click();
-                    } else {
-                      // Log a message if the element is not found
-                      console.log(
-                        "Skipper is searching The Seven Seas for the 'Skip' button ⛵"
-                      );
-                    }
-                  }
-
-                  // Call the clickElement function every 1000 milliseconds
-                  setInterval(clickElement, 1000);
-                }
-              },
+    chrome.storage.local.get(['isSkipperEnabled'], function(result) {
+      if (
+        result.isSkipperEnabled &&
+        changeInfo.status === 'complete' &&
+        tab.url &&
+        tab.url.includes('https://www.youtube.com/')
+      ) {
+        // Execute a script in the updated tab
+        chrome.scripting.executeScript(
+          {
+            target: { tabId },
+            func: function () {
+              // Rest of the code...
             },
-            () => {
-              if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError.message);
-              }
+          },
+          () => {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError.message);
             }
-          );
-        }
-      });
-    }
+          }
+        );
+      }
+    });
   });
 })();
