9ef1d5a86f64f9a4780f86567f4f23e55bc7d779
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-05-24 22:29:19 -0700

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,12 +1,30 @@
-// Fetch the 'status' div element
-const statusDiv = document.getElementById('status');
+// Get tab buttons
+const optionsButton = document.getElementById('btn-options');
+const siteListButton = document.getElementById('btn-site-list');
+const moreButton = document.getElementById('btn-more');
 
-// Update the status display
-function updateUI(isEnabled) {
-  statusDiv.textContent = `Status: ${isEnabled ? 'Enabled' : 'Disabled'}`;
+// Set up event listeners
+optionsButton.addEventListener('click', () => openTab('Options'));
+siteListButton.addEventListener('click', () => openTab('SiteList'));
+moreButton.addEventListener('click', () => openTab('More'));
+
+// Tab switching function
+function openTab(tabName) {
+    var i, tabcontent, tablinks;
+    // Hide all elements with class="tabcontent"
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    // Remove the "active" class from all elements with class="tablinks"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    // Show the current tab and add the "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(`btn-${tabName.toLowerCase()}`).className += " active";
 }
 
-// Fetch and display the current status of Skipper when the popup is opened
-chrome.storage.local.get(['isSkipperEnabled'], function(result) {
-  updateUI(result.isSkipperEnabled);
-});
+// Start with the 'Options' tab open
+openTab('Options');
