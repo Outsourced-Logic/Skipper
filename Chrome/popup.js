29a6b06ab0314fb265889236bfa7e5a32e7f1b61
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-05-24 19:42:08 -0700

    Removing enabled/disabled button

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,34 +1,14 @@
-// When the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-    const statusDiv = document.getElementById('status');
-    const toggleButton = document.getElementById('toggle');
-    
-    // Fetch the 'enabled' value from storage
-    chrome.storage.sync.get('enabled', function(data) {
-        const enabled = data.enabled;
-        updateUI(enabled);
-    });
+// Fetch the 'status' div and 'toggle' button elements
+const statusDiv = document.getElementById('status');
+const toggleButton = document.getElementById('toggle');
 
-    // Attach a click event listener to the button
-    toggleButton.addEventListener('click', function() {
-        // Fetch the 'enabled' value from storage
-        chrome.storage.sync.get('enabled', function(data) {
-            const enabled = data.enabled;
-            // Update the 'enabled' value in storage
-            chrome.storage.sync.set({enabled: !enabled}, function() {
-                updateUI(!enabled);
-            });
-        });
-    });
+// Update the status display and the toggle button text
+function updateUI(isEnabled) {
+  statusDiv.textContent = `Status: ${isEnabled ? 'Enabled' : 'Disabled'}`;
+  toggleButton.textContent = `${isEnabled ? 'Disable' : 'Enable'}`;
+}
 
-    // Function to update the UI
-    function updateUI(enabled) {
-        if (enabled) {
-            statusDiv.textContent = 'Status: Enabled';
-            toggleButton.textContent = 'Disable';
-        } else {
-            statusDiv.textContent = 'Status: Disabled';
-            toggleButton.textContent = 'Enable';
-        }
-    }
+// Fetch and display the current status of Skipper when the popup is opened
+chrome.storage.local.get(['isSkipperEnabled'], function(result) {
+  updateUI(result.isSkipperEnabled);
 });
