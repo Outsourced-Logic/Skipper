b9066f98254be2d38e6fe77f6b85f292e6465341
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-05-25 18:33:59 -0700

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,33 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const tablinks = document.getElementsByClassName("tablinks");
+// Get all the tab buttons and tab content divs
+var tablinks = document.getElementsByClassName("tablinks");
+var tabcontent = document.getElementsByClassName("tabcontent");
 
-  // Tab switching function
-  const openTab = (tabName) => {
-    var i, tabcontent;
+// Hide all tab content divs initially
+for (var i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+}
 
-    // Hide all elements with class="tabcontent"
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+// Attach a click event listener to each tab button
+for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].addEventListener("click", function() {
+        // Remove the active class from all tab buttons
+        for (var j = 0; j < tablinks.length; j++) {
+            tablinks[j].className = tablinks[j].className.replace(" active", "");
+        }
 
-    // Show the current tab
-    document.getElementById(tabName).style.display = "block";
-  }
+        // Hide all tab content divs
+        for (var j = 0; j < tabcontent.length; j++) {
+            tabcontent[j].style.display = "none";
+        }
 
-  // Event listeners for tab buttons
-  for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].addEventListener('click', function() {
-      // Remove active class from other tabs
-      for (let j = 0; j < tablinks.length; j++) {
-        tablinks[j].className = tablinks[j].className.replace(" active", "");
-      }
-      // Add active class to clicked tab and open it
-      this.className += " active";
-      openTab(this.id.split('-')[1]);
+        // Show the content div that corresponds to the clicked tab
+        document.getElementById(this.id.replace("btn-", "")).style.display = "block";
+
+        // Add the active class to the clicked tab button
+        this.className += " active";
     });
-  }
+}
 
-  // Open the 'Options' tab by default
-  document.getElementById('btn-options').click();
-});
+// Show the first tab content div and add the active class to the first tab button by default
+if (tabcontent[0]) {
+    tabcontent[0].style.display = "block";
+}
+if (tablinks[0]) {
+    tablinks[0].className += " active";
+}
