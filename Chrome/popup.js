4af3b7581e7afa4bd7f1a4a4585ab048dc307947
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-05-25 18:45:01 -0700

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,37 +1,30 @@
-// Get all the tab buttons and tab content divs
-var tablinks = document.getElementsByClassName("tablinks");
-var tabcontent = document.getElementsByClassName("tabcontent");
+document.addEventListener('DOMContentLoaded', function() {
+    var tablinks = document.getElementsByClassName("tablinks");
+    var tabcontent = document.getElementsByClassName("tabcontent");
 
-// Hide all tab content divs initially
-for (var i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-}
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-// Attach a click event listener to each tab button
-for (var i = 0; i < tablinks.length; i++) {
-    tablinks[i].addEventListener("click", function() {
-        // Remove the active class from all tab buttons
-        for (var j = 0; j < tablinks.length; j++) {
-            tablinks[j].className = tablinks[j].className.replace(" active", "");
-        }
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].addEventListener("click", function() {
+            for (var j = 0; j < tablinks.length; j++) {
+                tablinks[j].className = tablinks[j].className.replace(" active", "");
+            }
 
-        // Hide all tab content divs
-        for (var j = 0; j < tabcontent.length; j++) {
-            tabcontent[j].style.display = "none";
-        }
+            for (var j = 0; j < tabcontent.length; j++) {
+                tabcontent[j].style.display = "none";
+            }
 
-        // Show the content div that corresponds to the clicked tab
-        document.getElementById(this.id.replace("btn-", "")).style.display = "block";
+            document.getElementById(this.id.replace("btn-", "")).style.display = "block";
+            this.className += " active";
+        });
+    }
 
-        // Add the active class to the clicked tab button
-        this.className += " active";
-    });
-}
-
-// Show the first tab content div and add the active class to the first tab button by default
-if (tabcontent[0]) {
-    tabcontent[0].style.display = "block";
-}
-if (tablinks[0]) {
-    tablinks[0].className += " active";
-}
+    if (tabcontent[0]) {
+        tabcontent[0].style.display = "block";
+    }
+    if (tablinks[0]) {
+        tablinks[0].className += " active";
+    }
+}, false);
