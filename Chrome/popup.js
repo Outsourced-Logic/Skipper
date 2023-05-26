4614a29b52fcd52e1e3d83488e9bca1c5a5b6b61
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-05-25 18:51:24 -0700

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -2,25 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
     var tablinks = document.getElementsByClassName("tablinks");
     var tabcontent = document.getElementsByClassName("tabcontent");
 
-    for (var i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    function switchTab() {
+        // Hide all tab content divs
+        for (var j = 0; j < tabcontent.length; j++) {
+            tabcontent[j].style.display = "none";
+        }
+
+        // Remove the active class from all tab buttons
+        for (var j = 0; j < tablinks.length; j++) {
+            tablinks[j].className = tablinks[j].className.replace(" active", "");
+        }
+
+        // Show the content div that corresponds to the clicked tab
+        document.getElementById(this.id.replace("btn-", "")).style.display = "block";
+
+        // Add the active class to the clicked tab button
+        this.className += " active";
     }
 
     for (var i = 0; i < tablinks.length; i++) {
-        tablinks[i].addEventListener("click", function() {
-            for (var j = 0; j < tablinks.length; j++) {
-                tablinks[j].className = tablinks[j].className.replace(" active", "");
-            }
-
-            for (var j = 0; j < tabcontent.length; j++) {
-                tabcontent[j].style.display = "none";
-            }
-
-            document.getElementById(this.id.replace("btn-", "")).style.display = "block";
-            this.className += " active";
-        });
+        tablinks[i].addEventListener("click", switchTab);
     }
 
+    // Show the first tab content div and add the active class to the first tab button by default
     if (tabcontent[0]) {
         tabcontent[0].style.display = "block";
     }
