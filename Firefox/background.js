f5d2a802ba999acb212a5b6b86e982f79edd3269
Outsourced-Logic <63562832+Outsourced-Logic@users.noreply.github.com>
2023-11-10 18:12:27 -0800

    Updated button fix
    Allows skipper to recognize both original and updated skip ad buttons

--- Firefox/background.js
+++ Firefox/background.js
@@ -6,21 +6,28 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         chrome.tabs.executeScript(tabId, {code: `if (!window.myExtensionScriptInjected) {
             // Set a flag to indicate that the script has been injected
             window.myExtensionScriptInjected = true;
-            
+
             // Define a function to click an element
             function clickElement() {
-                // Find the 'Skip' button element
-                var element = document.querySelector(".ytp-ad-skip-button.ytp-button");
-                // Check if the element exists and is visible
-                if (element && element.offsetParent !== null) {
-                    // Click the element
-                    element.click();
+                // Try to find the new 'Skip Ad' button
+                var newElement = document.querySelector('.ytp-ad-skip-button-modern.ytp-button');
+                // Try to find the original 'Skip Ad' button
+                var originalElement = document.querySelector('.ytp-ad-skip-button.ytp-button');
+
+                // Determine which element is visible and click it
+                var elementToClick = newElement && newElement.offsetParent !== null ? newElement : 
+                                     originalElement && originalElement.offsetParent !== null ? originalElement : 
+                                     null;
+
+                if (elementToClick) {
+                    // Click the visible element
+                    elementToClick.click();
                 } else {
-                    // Log a message if the element is not found
+                    // Log a message if neither element is found
                     console.log("Skipper is searching The Seven Seas for the 'Skip' button ⛵");
                 }
             }
-            
+
             // Call the clickElement function every 1000 milliseconds
             setInterval(clickElement, 1000);
         }`});
